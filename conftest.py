a421f36a9348e6294660ecfc711e1264ce51c7bd
Anastasiya Kuznetsova <forquest@icloud.com>
2025-02-13 15:41:39 +0300

    Внесла изменения в фикстуру - yield

--- conftest.py
+++ conftest.py
@@ -2,20 +2,9 @@ import pytest
 from selene import browser
 
 
-def pytest_addoption(parser):
-    """Добавляем опции для изменения размера окна"""
-    parser.addoption("--width", action="store", default=1280, help="Width of the browser window")
-    parser.addoption("--height", action="store", default=720, help="Height of the browser window")
-
-
 @pytest.fixture(scope="function", autouse=True)
-def setup_browser(pytestconfig):
-    """Фикстура для настройки размера окна браузера"""
-    width = pytestconfig.getoption("--width")
-    height = pytestconfig.getoption("--height")
-
-    browser.config.window_width = int(width)
-    browser.config.window_height = int(height)
-
+def setup_browser():
+    browser.config.window_width = 1280
+    browser.config.window_height = 1220
     yield
     browser.quit()
--- google_should_find_selene.py
+++ google_should_find_selene.py
@@ -6,11 +6,10 @@ def test_succesfull_search_yaru():
     browser.element('[name="text"]').should(be.blank).type('yashaka/selene').press_enter()
    # browser.element('html').should(have.text('About this page'))
     browser.element('[id="search-result"]').should(have.text("User-oriented Web UI browser tests in Python"))
-    browser.quit()
+
 
 def test_succesfull_nothing_found():
     browser.open('https://ya.ru')
     browser.element('[name="text"]').should(be.blank).type('??').press_enter()
     browser.element('[class="content__left"]').should(be.present)
     #browser.element('[id="fvzhbb8-PJgm-FE"]').should(have.text("Ничего не нашли"))
-    browser.quit()
