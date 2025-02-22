0ebdde9de9f7082dc8b61605ae052919a517bec0
Anastasiya Kuznetsova <forquest@icloud.com>
2025-02-22 12:58:41 +0300

    Добавила опиcание занятия в readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# AnastasiyaKZC-Homework_L3
+# Homework_L3 - Погружаемся в инструментарий и библиотеки. 
+
+1. Git & Github – работаем из Pycharm
+2. Подключение зависимостей - requirements.txt
+3. Основные зависимости (Pytest, Selene)
+4. Фикстуры Pytest
--- conftest.py
+++ conftest.py
@@ -5,6 +5,6 @@ from selene import browser
 @pytest.fixture(scope="function", autouse=True)
 def setup_browser():
     browser.config.window_width = 1280
-    browser.config.window_height = 1220
+    browser.config.window_height = 220
     yield
     browser.quit()
